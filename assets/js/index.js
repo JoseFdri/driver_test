3617f5ca96adfa87332f84f31b29085d49a3f0f0
jose Rodriguez G <josefernaguillen@gmail.com>
2024-01-02 01:58:02 +1300

    init

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new fullpage('#fullpage', {
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true
+    });
+  }, false);
